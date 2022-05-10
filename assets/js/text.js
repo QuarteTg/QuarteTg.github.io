b0a1f56f34ab622824f6114aef922872b4931060
Quarte <104691705+QuarteTg@users.noreply.github.com>
2022-05-10 22:38:11 +0700

    Add files via upload

---
+++ assets/js/text.js
@@ -0,0 +1,11 @@
+$(function () {
+    $(".typed").typed({
+        strings: ["Крым ничей", "#НетВойне", "ANTI-SCAMMMER", "Python Developer", "coder", "vzlom jopi", "hmmm...", "bruh", "Hack system...", "h@Cк3D!"],
+        typeSpeed: 30,
+        backSpeed: 0,
+        cursorChar: ['❚'],
+        smartBackspace: true,
+        fadeOut: true,
+        loop: true,
+    });
+});
