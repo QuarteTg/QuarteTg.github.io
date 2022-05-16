e3882c1af78fffdfb0162cf7924500ce981fa64f
Quarte <104691705+QuarteTg@users.noreply.github.com>
2022-05-16 08:50:05 +0700

    Update text.js

--- assets/js/text.js
+++ assets/js/text.js
@@ -1,6 +1,6 @@
 $(function () {
     $(".typed").typed({
-        strings: ["Quarte", "Hello world!", "Telegram Developer", "nedoкодер", "hmmm...", "bruh", "Kto eto?", "h@Cк3D!"],
+        strings: ["Quarte", "Termuxtu3tor", "Hello world!", "Telegram Developer", "nedoкодер", "hmmm...", "bruh", "Kto eto?", "h@Cк3D!"],
         typeSpeed: 30,
         backSpeed: 0,
         cursorChar: ['❚'],
